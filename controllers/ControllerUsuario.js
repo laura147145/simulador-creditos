e6b235d2e3cb174c1563ff4faffdf99a6a9dc252
laura147145 <laura147145>
2024-11-14 15:57:05 -0500

    Actualizacion

--- controllers/ControllerUsuario.js
+++ controllers/ControllerUsuario.js
@@ -2,6 +2,9 @@ import { usuarios } from "../models/ModelUsuario.js";
 
 export function login() {
     console.log(usuarios);
+    let loginUsuario = document.getElementById();
+    let loginContrasena = document.getElementById();
+usuarios.some(function(){});
     
 }
 
@@ -9,7 +12,7 @@ export function register() {
     let registroNombre = document.getElementById("register-nombre").value;
     let registroUsuario = document.getElementById("register-usuario").value;
     let registroCorreo = document.getElementById("register-correo").value;
-    let registroContraseña = document.getElementById("register-contrasena").value;
+    let registroCo ntraseña = document.getElementById("register-contrasena").value;
     let registroConfirmar = document.getElementById("register-confirmar").value;
     let newRegistro = {
         nombre: registroNombre,
@@ -18,9 +21,13 @@ export function register() {
         contrasena: registroContraseña,
         confirmar: registroConfirmar
     };
-    console.log(newRegistro);
+    console.log(usuarios);
+    usuarios.unshift(newRegistro);
 
     usuarios.push(newRegistro);
+    document.getElementById("from-register").style.display = "none";
+    document.getElementById("form-login").style.display = "flex"
+
     
     
 }
